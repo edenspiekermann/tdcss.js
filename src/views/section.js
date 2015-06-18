19c5af6778d8e606824987fa8fa432bdeb00080b
marcohamersma <hello@marcohamersma.nl>
2015-06-18 17:46:10 +0200

    Code conventions

--- src/views/section.js
+++ src/views/section.js
@@ -2,24 +2,24 @@ var Backbone = require('backbone');
 var Handlebars = require('handlebars');
 
 module.exports = Backbone.View.extend({
-  className: "tdcss-section",
+    className: "tdcss-section",
 
-  template: require('./section.hbs'),
+    template: require('./section.hbs'),
 
-  initialize: function() {
-    if (this.model.get('wip')) {
-      this.className += ' wip';
+    initialize: function() {
+        if (this.model.get('wip')) {
+            this.className += ' wip';
+        }
+    },
+
+    attributes: function() {
+        return {
+          id: this.model.toUniqueID()
+        };
+    },
+
+    render: function() {
+        this.$el.html(this.template(this.model.attributes));
+        return this;
     }
-  },
-
-  attributes: function() {
-    return {
-      id: this.model.toUniqueID()
-    };
-  },
-
-  render: function() {
-    this.$el.html(this.template(this.model.attributes));
-    return this;
-  }
 });
